a441cdd74335bee4beba13de9f883e693f8cb5f3
hangyeol0531 <jhan0531@naver.com>
2025-01-05 16:01:57 +0900

    springboot init

---
+++ gdgoc/src/test/java/inha/gdgoc/GdgocApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.gdgoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
