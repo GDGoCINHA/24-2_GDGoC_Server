902960868c7a16f0c3ccf1d80a0abdfcd0ed9532
kaswhy <sxshx818@naver.com>
2025-01-21 02:16:04 +0900

    feat(Recruit): 모집 폼에 대한 답변 엔티티 구현

--- gdgoc/src/main/java/inha/gdgoc/domain/recruit/entity/Answer.java
+++ gdgoc/src/main/java/inha/gdgoc/domain/recruit/entity/Answer.java
@@ -1,12 +1,15 @@
-package inha.gdgoc.domain.question.entity;
+package inha.gdgoc.domain.recruit.entity;
 
+import inha.gdgoc.domain.question.entity.Question;
+import inha.gdgoc.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -23,32 +26,24 @@ import org.springframework.data.annotation.LastModifiedDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Question {
+public class Answer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "type", nullable = false)
-    private Type type;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @JdbcTypeCode(SqlTypes.JSON)
-    @Column(name = "data_type", nullable = false)
-    private DataType dataType;
-
-    @Column(name = "order", nullable = false)
-    private int order;
-
-    @Column(name = "content", nullable = false)
-    private String content;
-
-    @Column(name = "is_required", nullable = false)
-    private boolean isRequired;
-
-    @Column(name = "is_used", nullable = false)
-    private boolean isUsed;
+    @Column(name = "content", nullable = true)
+    private AnswerData answerData;
 
     @CreatedDate
     @Column(name = "created_at")
