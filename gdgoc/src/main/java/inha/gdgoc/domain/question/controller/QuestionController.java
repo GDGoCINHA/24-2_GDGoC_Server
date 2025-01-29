04a8665716a9e22603eeb4331dcc9bb30bc13160
kaswhy <sxshx818@naver.com>
2025-01-30 00:19:43 +0900

    feat(question): 변동 가능성 있는 질문 등록 api 구현

---
+++ gdgoc/src/main/java/inha/gdgoc/domain/question/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package inha.gdgoc.domain.question.controller;
+
+import inha.gdgoc.domain.question.dto.QuestionRequestDto;
+import inha.gdgoc.domain.question.entity.Question;
+import inha.gdgoc.domain.question.service.QuestionService;
+import inha.gdgoc.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/question")
+    public ApiResponse<Long> createQuestion(@RequestBody QuestionRequestDto questionRequestDto) {
+        Question savedQuestion = questionService.save(questionRequestDto);
+
+        return ApiResponse.success(savedQuestion.getId(), "질문이 성공적으로 생성되었습니다.");
+    }
+
+}
---
+++ gdgoc/src/main/java/inha/gdgoc/domain/question/dto/QuestionRequestDto.java
@@ -0,0 +1,29 @@
+package inha.gdgoc.domain.question.dto;
+
+import inha.gdgoc.domain.question.entity.Question;
+import inha.gdgoc.domain.question.enums.InputType;
+import inha.gdgoc.domain.question.enums.SurveyType;
+import lombok.Data;
+
+@Data
+public class QuestionRequestDto {
+    private SurveyType surveyType;
+    private InputType inputType;
+    private int order;
+    private String title;
+    private String description;
+    private boolean isRequired;
+    private boolean isUsed;
+
+    public Question toEntity() {
+        return Question.builder()
+                .surveyType(surveyType)
+                .inputType(inputType)
+                .order(order)
+                .title(title)
+                .description(description)
+                .isRequired(isRequired)
+                .isUsed(isUsed)
+                .build();
+    }
+}
