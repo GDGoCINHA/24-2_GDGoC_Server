2df2125aab3dc564e8232215d7783599e994503e
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-02-09 00:30:16 +0900

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Spring Boot 애플리케이션 실행 중..."
+
+# 애플리케이션 경로 이동
+cd /home/ec2-user/app
+
+# Spring Boot 실행 (백그라운드 모드)
+nohup java -jar build/libs/*.jar > /dev/null 2>&1 &
+
+echo "Spring Boot 애플리케이션 실행 완료!"
