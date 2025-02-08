9a007003fdc55ae2c9df52f170f72f7cda91e3f4
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-02-09 01:40:40 +0900

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -10,3 +10,17 @@ if ! java -version 2>&1 | grep -q "17"; then
 else
   echo "Java 17 JDK가 이미 설치되어 있습니다."
 fi
+
+#!/bin/bash
+echo "🚀 S3에서 다운로드한 ZIP 파일 압축 해제 중..."
+cd /home/ubuntu/gdgoc
+
+# 기존 파일 삭제
+rm -rf /home/ubuntu/gdgoc/*
+
+# ZIP 파일 압축 해제
+unzip /home/ubuntu/gdgoc/deploy.zip -d /home/ubuntu/gdgoc
+chmod +x /home/ubuntu/gdgoc/scripts/start.sh
+
+echo "✅ 압축 해제 완료!"
+
