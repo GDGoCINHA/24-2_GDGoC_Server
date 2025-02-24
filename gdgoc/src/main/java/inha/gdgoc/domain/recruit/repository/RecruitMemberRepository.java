83263185efc132cf9f8308fbc4766d2e597ab9e6
kaswhy <sxshx818@naver.com>
2025-02-24 20:15:48 +0900

    feat(Recruit): 중복 확인 리포지토리 추가

--- gdgoc/src/main/java/inha/gdgoc/domain/recruit/repository/RecruitMemberRepository.java
+++ gdgoc/src/main/java/inha/gdgoc/domain/recruit/repository/RecruitMemberRepository.java
@@ -4,4 +4,6 @@ import inha.gdgoc.domain.recruit.entity.RecruitMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecruitMemberRepository extends JpaRepository<RecruitMember, Long> {
+    boolean existsByStudentId(String studentId);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
