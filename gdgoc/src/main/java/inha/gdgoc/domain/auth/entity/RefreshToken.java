0f1768f9cbbc74fb096cf595de57f407577c61cd
kaswhy <sxshx818@naver.com>
2025-03-31 18:14:45 +0900

    fix(auth): refresh token 객체에서 user 예약어라 안 되는 부분 컬럼명 변경

--- gdgoc/src/main/java/inha/gdgoc/domain/auth/entity/RefreshToken.java
+++ gdgoc/src/main/java/inha/gdgoc/domain/auth/entity/RefreshToken.java
@@ -29,7 +29,7 @@ public class RefreshToken {
     private String token;
 
     @OneToOne
-    @JoinColumn(name = "user", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     private LocalDateTime expiryDate;
