230e4d255e870c228b2d180b5568cd34fe14854a
kaswhy <sxshx818@naver.com>
2025-03-31 22:48:05 +0900

    feat(config): jwt 필터링 시 로그 추가

--- gdgoc/src/main/java/inha/gdgoc/config/TokenAuthenticationFilter.java
+++ gdgoc/src/main/java/inha/gdgoc/config/TokenAuthenticationFilter.java
@@ -13,11 +13,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
@@ -32,14 +34,19 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         String token = getAccessToken(request);
+        log.info("요청 URI: {}, 추출된 access token: {}", request.getRequestURI(), token);
 
         if (token != null) {
             try {
                 Authentication authentication = tokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("JWT 인증 성공: {}", authentication.getName());
             } catch (Exception e) {
                 SecurityContextHolder.clearContext();
+                log.warn("JWT 인증 실패: {}", e.getMessage());
             }
+        } else {
+            log.info("Authorization 헤더 없음 → 인증 시도 안함");
         }
 
         filterChain.doFilter(request, response);
