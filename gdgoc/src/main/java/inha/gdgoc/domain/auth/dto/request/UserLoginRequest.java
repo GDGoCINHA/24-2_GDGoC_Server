ea24dd605a7cb9730577b392912ab65c37b142d7
kaswhy <sxshx818@naver.com>
2025-04-08 21:03:38 +0900

    feat(gdgoc): api 공동 응답에서 의미 중복되는 부분 삭제 및 오류 시 응답 분리

---
+++ gdgoc/src/main/java/inha/gdgoc/domain/auth/dto/request/UserLoginRequest.java
@@ -0,0 +1,8 @@
+package inha.gdgoc.domain.auth.dto.request;
+
+import inha.gdgoc.global.common.BaseEntity;
+
+public class UserLoginRequest extends BaseEntity {
+    private String id;
+    private String password;
+}
