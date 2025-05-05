c04b065440539adc5a233226450c364e48ec04bf
kaswhy <sxshx818@naver.com>
2025-05-05 13:18:26 +0900

    feat(resource): s3 관련 설정 추가

--- gdgoc/src/main/java/inha/gdgoc/domain/resource/service/S3Service.java
+++ gdgoc/src/main/java/inha/gdgoc/domain/resource/service/S3Service.java
@@ -3,7 +3,7 @@ package inha.gdgoc.domain.resource.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +16,6 @@ public class S3Service {
 
     private final AmazonS3 amazonS3;
 
-    //TODO S3처리
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
