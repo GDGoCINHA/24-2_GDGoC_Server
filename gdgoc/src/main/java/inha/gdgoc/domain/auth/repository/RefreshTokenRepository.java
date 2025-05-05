f057ef86a79ce2cbf4b01e013c76c1d5ee334031
kaswhy <sxshx818@naver.com>
2025-05-05 17:54:25 +0900

    fix(auth): 토큰 제대로 삭제 안되는 문제 수정

--- gdgoc/src/main/java/inha/gdgoc/domain/auth/repository/RefreshTokenRepository.java
+++ gdgoc/src/main/java/inha/gdgoc/domain/auth/repository/RefreshTokenRepository.java
@@ -4,11 +4,15 @@ import inha.gdgoc.domain.auth.entity.RefreshToken;
 import inha.gdgoc.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUser(User user);
 
-    void deleteByUserIdAndToken(Long userId, String token); // 로그아웃할 때 이 토큰만 삭제
+    @Modifying
+    @Query("DELETE FROM RefreshToken r WHERE r.user.id = :userId AND r.token = :token")
+    void deleteByUserIdAndToken(Long user_id, String token); // 로그아웃할 때 이 토큰만 삭제
 }
