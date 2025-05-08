968c03c888ffcd7545974b7affc9c0e0f1be25a7
kaswhy <sxshx818@naver.com>
2025-05-09 00:16:03 +0900

    fix(gdgoc): 오류 코드 수정

--- gdgoc/src/main/java/inha/gdgoc/global/error/GlobalErrorCode.java
+++ gdgoc/src/main/java/inha/gdgoc/global/error/GlobalErrorCode.java
@@ -6,8 +6,10 @@ public enum GlobalErrorCode implements ErrorCode {
 
     // 400 Bad Request
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 경로의 파라미터는 올바른 형식이 아닙니다."),
-    INVALID_JWT_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰입니다."),
     INVALID_JSON_REQUEST(HttpStatus.BAD_REQUEST, "JSON 형식이 올바르지 않습니다."),
+
+    // 403 FORBIDDEN
+    INVALID_JWT_REQUEST(HttpStatus.FORBIDDEN, "잘못된 JWT 토큰입니다."),
 
     // 404 Not Found
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리소스입니다."),
