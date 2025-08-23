eb7e676c004f82e98a6efb500dd02b072a0052ba
kaswhy <sxshx818@naver.com>
2025-08-23 16:29:37 +0900

    fix(#70/.github): docker-compose 지정

--- gdgoc/deploy.prod.sh
+++ gdgoc/deploy.prod.sh
@@ -20,7 +20,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 # 기존 컨테이너 중지 및 삭제
-docker-compose down
+docker-compose -f docker-compose-prod.yml down
 
 # 사용되지 않는 컨테이너, 이미지, 네트워크, 볼륨 정리
 docker system prune -af
@@ -35,4 +35,4 @@ export $(grep -v '^#' .env | xargs)
 docker pull ${DOCKER_HUB_USERNAME}/gdgoc-be-app:latest
 
 # 컨테이너 실행
-docker-compose --env-file .env up -d
+docker-compose -f docker-compose-prod.yml --env-file .env up -d
