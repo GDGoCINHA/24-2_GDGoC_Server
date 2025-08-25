26431d52bd567662fe90a8d1bef5f34925a25201
kaswhy <sxshx818@naver.com>
2025-08-25 11:41:37 +0900

    refactor(#195/recruit): recruit 관련 도메인을 공통 api 응답 변경에 맞게 리팩토링

---
+++ src/main/java/inha/gdgoc/domain/recruit/controller/message/RecruitMemberMessage.java
@@ -0,0 +1,8 @@
+package inha.gdgoc.domain.recruit.controller.message;
+
+public class RecruitMemberMessage {
+    public static final String MEMBER_SAVE_SUCCESS = "성공적으로 해당 학기 멤버 가입을 완료했습니다.";
+    public static final String STUDENT_ID_DUPLICATION_CHECK_SUCCESS = "성공적으로 학번 중복 조회를 완료했습니다.";
+    public static final String PHONE_NUMBER_DUPLICATION_CHECK_SUCCESS = "성공적으로 전화번호 중복 조회를 완료했습니다.";
+    public static final String MEMBER_RETRIEVED_SUCCESS = "성공적으로 특정 멤버의 지원서를 조회했습니다.";
+}
