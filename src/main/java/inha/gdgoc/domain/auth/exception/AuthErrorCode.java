0a42e2cc839a01ac97a2a3f88bdeda4b154bfc41
kaswhy <sxshx818@naver.com>
2025-08-25 11:05:12 +0900

    refactor(#195/auth): AuthController 바뀐 공통 응답에 맞게 리팩토링

--- src/main/java/inha/gdgoc/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/inha/gdgoc/domain/auth/exception/AuthErrorCode.java
@@ -5,9 +5,15 @@ import org.springframework.http.HttpStatus;
 
 public enum AuthErrorCode implements ErrorCode {
 
+    // 401 Unauthorized
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+
     // 403 Forbidden
     INVALID_COOKIE(HttpStatus.FORBIDDEN, "Refresh Token 이 비어있습니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "잘못된 Refresh Token 값입니다.");
+    INVALID_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "잘못된 Refresh Token 값입니다."),
+
+    // 404 Not Found
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다");
 
     private final HttpStatus status;
     private final String message;
