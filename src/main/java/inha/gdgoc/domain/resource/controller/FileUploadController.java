8c57cf8fdd4a88c9bc5b9448b8f7b4a05f56896b
pwc2002 <pwc2002@naver.com>
2025-09-07 14:12:22 +0900

    파일 업로드 API 수정: @RequestParam에서 @RequestPart로 변경하고, Swagger 문서화 추가

--- src/main/java/inha/gdgoc/domain/resource/controller/FileUploadController.java
+++ src/main/java/inha/gdgoc/domain/resource/controller/FileUploadController.java
@@ -1,13 +1,15 @@
 package inha.gdgoc.domain.resource.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import inha.gdgoc.domain.resource.enums.S3KeyType;
 import inha.gdgoc.domain.resource.service.S3Service;
 import inha.gdgoc.global.dto.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,9 +22,10 @@ public class FileUploadController {
 
     private record UploadResponse(String url) {}
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "파일 업로드", description = "multipart/form-data로 파일 업로드 후 S3 URL 반환")
     public ResponseEntity<ApiResponse<UploadResponse, Void>> upload(
-        @RequestParam("file") MultipartFile file
+        @RequestPart("file") MultipartFile file
     ) throws Exception {
         String key = s3Service.upload(0L, S3KeyType.study, file);
         String url = s3Service.getS3FileUrl(key);
